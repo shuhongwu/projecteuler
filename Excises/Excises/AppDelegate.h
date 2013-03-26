70ff94767fc1b4ac25a9ff9add2e5da5ec4de9fc
AlexWu <ws169144@163.com>
2013-03-26 21:39:11 +0800

    have done Problem 4,3,2,1

---
+++ Excises/Excises/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Excises
+//
+//  Created by Alex on 3/22/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Excises/Excises/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Excises
+//
+//  Created by Alex on 3/22/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
