614650eeef7ef29a45696d1758da37be7f7a264b
AlexWu <ws169144@163.com>
2013-03-27 15:00:14 +0800

    6th todo

--- Excises/Excises/AppDelegate.m
+++ Excises/Excises/AppDelegate.m
@@ -118,7 +118,7 @@
      2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
      
      What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-     */
+     
     
     unsigned int i = 2500;
 	unsigned int j = 0;
@@ -130,6 +130,20 @@
 		}
 	}while(j<20);
 	printf("result: %d", i);
+    */
+    
+    /*
+     The sum of the squares of the first ten natural numbers is,
+     
+     12 + 22 + ... + 102 = 385
+     The square of the sum of the first ten natural numbers is,
+     
+     (1 + 2 + ... + 10)2 = 552 = 3025
+     Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+     
+     Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+     */
+    
     
     
     
